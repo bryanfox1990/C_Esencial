a01835d43e774243378135cb66879ac7fabdff99
bryanfox1990 <44558368+bryanfox1990@users.noreply.github.com>
2025-10-04 16:37:39 -0500

    Create holamundo.c
    ejercicio para mostrar un hola mundo

---
+++ holamundo.c
@@ -0,0 +1,18 @@
+/*
+ * Curso: Integracion con Git y GitHub en Visual Studio
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+// Escribe un programa que muestre por pantalla el mensaje "Hola Mundo!"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	cout << "Hola Mundo!" << endl;
+
+	return 0;
+}
